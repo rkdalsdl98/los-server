2f5461fb613068688a66d2f96decfef10698ce48
KangMins <rkdalsdl98@naver.com>
2023-07-31 12:15:06 +0900

    '팀'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { TeamModule } from './team/team.module';
+import { NotifyModule } from './notify/notify.module';
 
 @Module({
-  imports: [EventsModule, TeamModule],
+  imports: [EventsModule, TeamModule, NotifyModule],
   controllers: [AppController],
   providers: [AppService],
 })
